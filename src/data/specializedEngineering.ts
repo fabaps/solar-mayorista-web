e0ea1cef27d5f0305f49ba08756a04beaed3d8d5
cris-01000011 <cris.01000011@gmail.com>
2026-05-04 10:15:46 -0600

    add specializedEngineering section

--- src/data/specializedEngineering.ts
+++ src/data/specializedEngineering.ts
@@ -1,7 +1,7 @@
 export const SPECIALIZED_ENGINEERING = [
   {
     img: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg",
-    title: "Bombeo en AC y DC",
+    title: "Bombeo en AC",
     description: "Generación de energía limpia y eficiente.",
   },
   {
@@ -17,6 +17,11 @@ export const SPECIALIZED_ENGINEERING = [
   {
     img: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg",
     title: "Atado a red industrial o comercial",
+    description: "Generación de energía limpia y eficiente.",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Photovoltaik_Dachanlage_Hannover_-_Schwarze_Heide_-_1_MW.jpg",
+    title: "Bombeo en DC",
     description: "Generación de energía limpia y eficiente.",
   },
 ] as const;
